89e16fa950c2e738ee8158cb5259d58b9a05edb8
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-06-10 21:39:31 -0300

    punto-6

--- common/common.routes.config.ts
+++ common/common.routes.config.ts
@@ -1,13 +1,15 @@
-import express from "express"
-export class commonRoutesConfig {
-  app : express.Application;
-  name :string;
+import express from 'express';
+export abstract class CommonRoutesConfig {
+    app: express.Application;
+    name: string;
 
-  constructor(app: express.Application, name : string) {
-    this.app=app;
-    this.name=name;
-  }
-  getName(){
-    return this.name
-  }
+    constructor(app: express.Application, name: string) {
+        this.app = app;
+        this.name = name;
+        this.configureRoutes();
+    }
+    getName() {
+        return this.name;
+    }
+    abstract configureRoutes(): express.Application;
 }
