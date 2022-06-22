23d7f2114421471ac4529a829caba1c003f7a8ad
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-06-22 20:14:36 -0300

    feat:CRUD with mongoose

--- users/daos/users.dao.ts
+++ users/daos/users.dao.ts
@@ -1,89 +1,84 @@
+import shortid from 'shortid';
+import debug from 'debug';     
+
+
+import mongooseService from '../../common/services/mongoose.service';
 import { CreateUserDto } from '../dto/create.user.dto';
 import { PatchUserDto } from '../dto/patch.user.dto';
 import { PutUserDto } from '../dto/put.user.dto';
-import shortid from 'shortid';
-import debug from 'debug';
+
 
 const log: debug.IDebugger = debug('app:in-memory-dao');
 
 class UsersDao {
-    users: Array<CreateUserDto> = [];
+Users:CreateUserDto[] = []
+
+    Schema = mongooseService.getMongoose().Schema;
+
+userSchema = new this.Schema({
+    _id: String,
+    email: String,
+    password: { type: String, select: false },
+    firstName: String,
+    lastName: String,
+    permissionFlags: Number,
+}, { id: false });
+
+User = mongooseService.getMongoose().model('Users', this.userSchema);
 
     constructor() {
         log('Created new instance of UsersDao');
     }
-    async addUser(user: CreateUserDto) {
-    user.id = shortid.generate();
-    this.users.push(user);
-    return user.id;
+
+    async addUser(userFields: CreateUserDto) {
+    const userId = shortid.generate();
+    const user = new this.User({
+        _id: userId,
+        ...userFields,
+        permissionFlags: 1,
+    });
+    await user.save();
+    return userId;
+}
+
+async getUserByEmail(email: string) {
+    return this.User.findOne({ email: email }).exec();
+}
+
+async getUserById(userId: string) {
+    return this.User.findOne({ _id: userId }).populate('User').exec();
+}
+
+async getUsers(limit = 25, page = 0) {
+    return this.User.find()
+        .limit(limit)
+        .skip(limit * page)
+        .exec();
+}
+
+async updateUserById(
+    userId: string,
+    userFields: PatchUserDto | PutUserDto
+) {
+    const existingUser = await this.User.findOneAndUpdate(
+        { _id: userId }, // filtra
+        { $set: userFields },//valores
+        { new: true }// crea
+    ).exec();
+
+    return existingUser;
+}
+
+async removeUserById(userId: string) {
+    return this.User.deleteOne({ _id: userId }).exec();
+}
+
+
+   
 }
 
 // "leer todos los recursos" y "leer uno por ID". 
 
-async getUsers() {
-    return this.users;
-}
-
-async getUserById(userId: string) {
-    return this.users.find((user: { id: string }) => user.id === userId);
-}
-
-// actualizar significa sobrescribir el objeto completo (como un PUT) o sólo partes del objeto (como un PATCH):
-
-
-async putUserById(userId: string, user: PutUserDto) {
-    const objIndex = this.users.findIndex(
-        (obj: { id: string }) => obj.id === userId
-    );
-    this.users.splice(objIndex, 1, user);
-    return `${user.id} updated via put`;
-}
-
-async patchUserById(userId: string, user: PatchUserDto) {
-    const objIndex = this.users.findIndex(
-        (obj: { id: string }) => obj.id === userId
-    );
-    let currentUser = this.users[objIndex];
-    const allowedPatchFields = [
-        'password',
-        'firstName',
-        'lastName',
-        'permissionLevel',
-    ];
-    for (let field of allowedPatchFields) {
-        if (field in user) {
-            // @ts-ignore
-            currentUser[field] = user[field];
-        }
-    }
-    this.users.splice(objIndex, 1, currentUser);
-    return `${user.id} patched`;
-}
-// para eliminar un recurso, se verá así:
-
-
-async removeUserById(userId: string) {
-    const objIndex = this.users.findIndex(
-        (obj: { id: string }) => obj.id === userId
-    );
-    this.users.splice(objIndex, 1);
-    return `${userId} removed`;
-}
-async getUserByEmail(email: string) {
-    const objIndex = this.users.findIndex(
-        (obj: { email: string }) => obj.email === email
-    );
-    let currentUser = this.users[objIndex];
-    if (currentUser) {
-        return currentUser;
-    } else {
-        return null;
-    }
-}
-
-
-
-}
 
 export default new UsersDao();
 
